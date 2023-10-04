afa6dcfa52c6907f09313e85ebcaa2bdcd6b8041
Juraj Borza <juraj.borza@nike.sk>
2023-10-04 17:43:24 +0200

    Added existing project files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directories = Directory.GetDirectories(".");
+            foreach(var directory in directories)
+            {
+                Rename(directory);
+            }
+        }
+
+        private static void Rename(string directory)
+        {
+            var newName = directory.Substring(0, 10) + "0"+directory.Substring(10);
+            Directory.Move(directory,newName);
+            Console.WriteLine(newName);
+        }
+    }
+}
